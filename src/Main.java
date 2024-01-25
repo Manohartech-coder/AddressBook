a16bf4c8bdb3e36115d2660e142d0ab1ef24ebd7
Manohartech-coder <manoharreddyndl988@gmail.com>
2024-01-25 11:50:36 +0530

    to create person detail class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book");
+
+        Person p1= new Person();
+
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,66 @@
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String State;
+    private String email;
+    private int zip;
+    private int phNo;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getPhNo() {
+        return phNo;
+    }
+
+    public void setPhNo(int phNo) {
+        this.phNo = phNo;
+    }
+}
